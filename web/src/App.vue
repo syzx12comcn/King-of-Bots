92513c1578c1faa064a8d9b11a40382e68a54a83
liuzhicheng <1509244804@qq.com>
2022-09-06 22:53:59 +0800

    auth

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,10 +9,43 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+// import $ from 'jquery'
+
 export default{
   components:{
     NavBar
-}
+  },
+  setup(){
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "hah",
+    //     password: "pppp",
+    //     confirmedPassword: "pppp",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2Y2NjMGNjZGQwYjg0NzgyOGYzZjAwNTVkZTA3YmUyYSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY2MjQ3MTczNiwiZXhwIjoxNjYzNjgxMzM2fQ.W6yLpb5gQHCylTVN6uP_DyTi8Q_lnWQ906wy3SuJmt4"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+  }
+
 }
 </script>
 
