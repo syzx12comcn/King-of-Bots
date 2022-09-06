4f812e21ccb74b284cc4ae5f0a6b4e04c645e29a
liuzhicheng <1509244804@qq.com>
2022-09-06 15:11:17 +0800

    security

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,15 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(new BCryptPasswordEncoder().encode("abc"));
+        System.out.println(new BCryptPasswordEncoder().matches("abc", "$2a$10$MrgUN2MagDUJhuDJMY4aQ.W2WWHcPdiM6D.dbFD26VtJ6udkAVHum"));
     }
 
 }
